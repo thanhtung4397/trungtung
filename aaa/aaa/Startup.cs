e819d47ccb907cc4e57a8f92f323396947a890bf
thanhtung43973 <quy19970997@gmail.com>
2019-03-20 09:05:42 +0700

    Tung

---
+++ aaa/aaa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aaa.Startup))]
+namespace aaa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
